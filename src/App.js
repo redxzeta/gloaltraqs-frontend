f4e7bdc8e67360a1ebef176410ebd96efcadc202
redxzeta <nsuarez.22@gmail.com>
2020-04-26 22:18:31 -0700

    reverting

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ export default function App() {
     <Provider store={store}>
       <Router>
         <>
+          <Header />
           <div>
             {/*<div className="container-fluid" style={divStyle}>*/}
             <Switch>
