5a1d2a19f59f2e4ed3c8d245c173fb3d10a8d678
redxzeta <xzetanzero@gmail.com>
2021-01-26 18:39:35 -0800

    merging css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./styles.css";
-import "./bootstrap.min_7.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
